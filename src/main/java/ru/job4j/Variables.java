9c7cce1515100c39eac1e0b5aae080392c918f40
pavlutsky <pavlutsky@mail.ru>
2022-01-30 15:27:30 +0800

    main

--- src/main/java/ru/job4j/Variables.java
+++ src/main/java/ru/job4j/Variables.java
@@ -2,21 +2,30 @@ package ru.job4j;
 
 public class Variables {
     public static void main(String[] args) {
-        byte mem = 1;
-        System.out.println(mem);
-        short size = 1;
-        System.out.println(size);
-        int age = 18;
+        System.out.print("age = ");
+        byte age = 39;
         System.out.println(age);
-        long money = 900500;
-        System.out.println(money);
-        float length = 78.0332F;
-        System.out.println(length);
-        double distance = 100500.99;
-        System.out.println(distance);
-        boolean baby = true;
-        System.out.println(baby);
-        char gender = 'Y';
+        System.out.print("gender = ");
+        char gender = 'M';
         System.out.println(gender);
+        System.out.print("growth = ");
+        float growth = 1.83F;
+        System.out.println(growth);
+        System.out.print("number of bricks = ");
+        short bricks = 30720;
+        System.out.println(bricks);
+        System.out.print("number of bacteria = ");
+        long bacteria = 350000000;
+        System.out.println(bacteria);
+        System.out.print("distance to the moon = ");
+        double distance = 384467;
+        System.out.println(distance);
+        System.out.print("file size = ");
+        int size = 18000000;
+        System.out.println(size);
+        System.out.print("permission to work = ");
+        boolean permission = true;
+        System.out.println(permission);
+
     }
 }
