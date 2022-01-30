2e9df97a9475fc8076391e6b14d7318ca9a01377
pavlutsky <pavlutsky@mail.ru>
2022-01-30 14:05:42 +0800

    main

---
+++ src/main/java/ru/job4j/Variables.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        int age = 18;
+        System.out.println(age);
+        long money = 900500;
+        System.out.println(money);
+        float length = 78.0332F;
+        System.out.println(length);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean baby = true;
+        System.out.println(baby);
+        char gender = 'Y';
+        System.out.println(gender);
+    }
+}
