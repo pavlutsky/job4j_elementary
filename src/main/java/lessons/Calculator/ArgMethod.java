556a7217b15135895e6cc6d7f14a3be5232474a9
pavlutsky <pavlutsky@mail.ru>
2022-01-31 12:29:44 +0800

    main1

--- src/main/java/lessons/Calculator/ArgMethod.java
+++ src/main/java/lessons/Calculator/ArgMethod.java
@@ -2,12 +2,13 @@ package lessons.calculator;
 
 public class ArgMethod {
 
-    public static void hello(String name) {
-        System.out.println("Hello, " + name);
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
     }
 
     public static void main(String[] args) {
-        String name = "Roman Pavlutsky";
-        ArgMethod.hello(name);
+        String name = "Pavlutsky";
+        int age = 39;
+        ArgMethod.hello(name, age);
     }
 }
