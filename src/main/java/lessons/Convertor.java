36c288392488d32a54942041295e890e65d4d48c
pavlutsky <pavlutsky@mail.ru>
2022-01-31 12:51:46 +0800

    main1

---
+++ src/main/java/lessons/Convertor.java
@@ -0,0 +1,18 @@
+package lessons;
+
+public class Convertor {
+    public static float rubleToEuro(float value) {
+        return value / 87;
+    }
+
+    public static float rubleToDollar(float value) {
+        return value / 78;
+    }
+
+    public static void main(String[] args) {
+        float euro = rubleToEuro(174);
+        System.out.println("174 rubles are " + euro + " euro.");
+        float dollar = rubleToDollar(117);
+        System.out.println("117 rubles are " + dollar + " dollars.");
+    }
+}
