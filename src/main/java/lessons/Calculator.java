7dd5697e41a1863d121ee9f4d49982282e2d612b
pavlutsky <pavlutsky@mail.ru>
2022-01-31 11:06:45 +0800

    main

--- src/main/java/lessons/Calculator.java
+++ src/main/java/lessons/Calculator.java
@@ -1,23 +1,26 @@
 package lessons;
 
 public class Calculator {
+        public static void plus(int first, int second) {
+            int result = first + second;
+            System.out.println(result);
+        }
 
-    public static void plus(int first, int second) {
-        int result = first + second;
-        System.out.println(result);
-    }
-    public static void minus(int first, int second) {
-        int result = first - second;
-        System.out.println(result);
-    }
-    public static void time(int first, int second) {
-        int result = first * second;
-        System.out.println(result);
-    }
-    public static void div(int first, int second) {
-        int result = first / second;
-        System.out.println(result);
-    }
+        public static void minus(int first, int second) {
+            int result = first - second;
+            System.out.println(result);
+        }
+
+        public static void div(int first, int second) {
+            int result = first / second;
+            System.out.println(result);
+        }
+
+        public static void time(int first, int second) {
+            int result = first * second;
+            System.out.println(result);
+        }
+
     public static void main(String[] args) {
         Calculator.plus(100, 500);
         Calculator.plus(4, 2);
@@ -25,11 +28,11 @@ public class Calculator {
         Calculator.minus(100, 500);
         Calculator.minus(4, 2);
         Calculator.minus(3, 5);
+        Calculator.div(100, 500);
+        Calculator.div(4, 2);
+        Calculator.div(3, 5);
         Calculator.time(100, 500);
         Calculator.time(4, 2);
         Calculator.time(3, 5);
-        Calculator.div(1499, 500);
-        Calculator.div(4, 2);
-        Calculator.div(30, 5);
     }
-}
+    }
--- src/main/java/lessons/Echo.java
+++
@@ -1,4 +0,0 @@
-package lessons;
-
-public class Echo {
-}
