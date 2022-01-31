977a83e59c56bbccb7ed9c4383ce5d4aa144b1e3
pavlutsky <pavlutsky@mail.ru>
2022-01-31 20:03:15 +0800

    main

---
+++ src/main/java/lessons/Exercise.java
@@ -0,0 +1,12 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class Exercise {
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int value = in.nextInt();
+            System.out.println(value);
+        }
+    }
