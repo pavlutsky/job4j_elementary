fc681a561880ec8b2a046966813bb076de7686c0
pavlutsky <pavlutsky@mail.ru>
2022-02-01 17:12:34 +0800

    main

--- src/test/java/lessons/ConverterTest.java
+++ src/test/java/lessons/ConverterTest.java
@@ -1,13 +1,13 @@
 package lessons;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class ConverterTest {
 
     @Test
-    public void whenConvert140RblThen2Euro() {
-        float in = 140;
+    public void whenConvert174RblThen2Euro() {
+        float in = 174;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
         float eps = 0.0001f;
@@ -15,9 +15,9 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert180RblThen3Dlr() {
-        float in = 180;
-        float expected = 3;
+    public void whenConvert117RblThen1dot5Dlr() {
+        float in = 117;
+        double expected = 1.5;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
