9a84857e03e31b12e9efc58af20bf1783c9cf7dd
pavlutsky <pavlutsky@mail.ru>
2022-02-01 14:30:06 +0800

    main

--- src/main/java/lessons/Exercise.java
+++ src/main/java/lessons/Exercise.java
@@ -1,7 +1,12 @@
 package lessons;
 
-import java.util.Scanner;
-
 public class Exercise {
 
+    public static void main(String[] args) {
+    float in = 174;
+    float expected = 2;
+    float out = Convertor.rubleToEuro(in);
+    boolean passed = expected == out;
+        System.out.println("174 rubles are 2. Test result : " + passed);
+    }
 }
