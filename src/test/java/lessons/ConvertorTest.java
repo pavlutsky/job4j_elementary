95e2874bfdb0d3eae311ae0df74a7e88109034f3
pavlutsky <pavlutsky@mail.ru>
2022-02-01 16:18:34 +0800

    main

---
+++ src/test/java/lessons/ConvertorTest.java
@@ -0,0 +1,17 @@
+package lessons;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+public class ConvertorTest {
+
+    @Test
+    public void rubleToEuro() {
+    }
+
+    @Test
+    public void rubleToDollar() {
+    }
+}
