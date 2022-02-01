7e3b4579d6947292d845a56371eacd2146bb98a8
pavlutsky <pavlutsky@mail.ru>
2022-02-01 16:36:26 +0800

    main

--- src/main/java/lessons/Exercise.java
+++ src/main/java/lessons/Exercise.java
@@ -5,12 +5,12 @@ package lessons;
         public static void main(String[] args) {
             float in = 174;
             float expected = 2;
-            float out = Convertor.rubleToEuro(in);
+            float out = Converter.rubleToEuro(in);
             boolean passed = expected == out;
             System.out.println("174 rubles are 2. Test result : " + passed);
             in = 117;
             double expected2 = 1.5;
-            out = Convertor.rubleToDollar(in);
+            out = Converter.rubleToDollar(in);
             boolean passed2 = expected2 == out;
             System.out.println("117 rubles are 1.5. Test result : " + passed2);
         }
