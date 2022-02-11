6a0c47fbd9f7f00b796522e5787b218115a3cc8a
pavlutsky <pavlutsky@mail.ru>
2022-02-11 15:28:07 +0800

    Глупый бот

---
+++ src/main/java/lessons/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package lessons.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if (question.equals("Привет, Бот.")) {
+            rsl = "Привет, умник";
+        } else if (question.equals("Пока.")) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Какой сейчас год?");
+        System.out.println(rsl);
+    }
+}
