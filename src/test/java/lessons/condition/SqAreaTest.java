c3ec78e4da390b6d66e9a6a0c42994af81f1cfdc
pavlutsky <pavlutsky@mail.ru>
2022-02-11 13:36:10 +0800

    Площадь треугольника через периметр и коофициент

---
+++ src/test/java/lessons/condition/SqAreaTest.java
@@ -0,0 +1,34 @@
+package lessons.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP7K2Square2dot72() {
+        double expected = 2.72f;
+        int p = 7;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP6K4Square1dot96() {
+        double expected = 1.96f;
+        int p = 7;
+        double k = 4;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
