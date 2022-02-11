5569d939fe2f85d4d40818a99c10fd3d2edcc70d
pavlutsky <pavlutsky@mail.ru>
2022-02-11 18:38:47 +0800

    Упражнение

--- src/main/java/lessons/Exercise.java
+++ src/main/java/lessons/Exercise.java
@@ -3,15 +3,9 @@ package lessons;
     public class Exercise {
 
         public static void main(String[] args) {
-            float in = 174;
-            float expected = 2;
-            float out = Converter.rubleToEuro(in);
-            boolean passed = expected == out;
-            System.out.println("174 rubles are 2. Test result : " + passed);
-            in = 117;
-            double expected2 = 1.5;
-            out = Converter.rubleToDollar(in);
-            boolean passed2 = expected2 == out;
-            System.out.println("117 rubles are 1.5. Test result : " + passed2);
+            boolean first = false;
+            boolean second = false;
+            boolean join = first && second;
+            System.out.println(join);
         }
     }
