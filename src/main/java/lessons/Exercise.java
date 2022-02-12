9e9d8a090d9ea8263b42f46ba6137a657e406b0a
pavlutsky <pavlutsky@mail.ru>
2022-02-12 19:17:17 +0800

    Exercise

--- src/main/java/lessons/Exercise.java
+++ src/main/java/lessons/Exercise.java
@@ -2,10 +2,20 @@ package lessons;
 
     public class Exercise {
 
-        public static void main(String[] args) {
-            boolean first = false;
-            boolean second = false;
-            boolean join = first && second;
-            System.out.println(join);
+        public static double div(double y) {
+            double i = y / 6;
+            int l = (int) i;
+            if (l != i) {
+                System.out.println(y + " не делится на 6");
+            } else {
+                System.out.println(y + " делится на 6");
+            }
+            return i;
         }
+
+        public static void main(String[] args) {
+            double rsl = Exercise.div(573723768);
+            rsl = Exercise.div(573723761);
+        }
+
     }
