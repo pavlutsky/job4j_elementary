673c25cdf97b6de2f7a340e00d95ed56f86740aa
pavlutsky <pavlutsky@mail.ru>
2022-02-15 13:09:26 +0800

    Ипотека

---
+++ src/main/java/lessons/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package lessons.loop;
+
+public class Mortgage {
+
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            year++;
+            amount = amount + amount * percent / 100 - salary;
+        }
+        return year;
+    }
+}
