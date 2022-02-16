8b5407342c194db2560b0d962c65c72b520ec337
pavlutsky <pavlutsky@mail.ru>
2022-02-16 19:04:26 +0800

    Массив заполнен true или false

---
+++ src/main/java/lessons/array/Check.java
@@ -0,0 +1,12 @@
+package lessons.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        for (int i = 1; i < data.length; i++) {
+            if (data[0] != data[i]) {
+                return false;
+            }
+       }
+        return true;
+    }
+}
