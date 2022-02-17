f12af78e8e117b30b43e5b91afa597e6e304903a
pavlutsky <pavlutsky@mail.ru>
2022-02-17 13:45:41 +0800

    Сравнить последние элементы двух массивов

---
+++ src/main/java/lessons/array/EqLast.java
@@ -0,0 +1,8 @@
+package lessons.array;
+
+public class EqLast {
+
+    public static boolean check(int[] left, int[] right) {
+        return left[left.length - 1] == right[right.length - 1];
+    }
+}
