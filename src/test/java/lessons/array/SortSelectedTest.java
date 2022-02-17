e94b12745c78adf7b9986e08867a2ed633b92ecc
pavlutsky <pavlutsky@mail.ru>
2022-02-17 17:45:56 +0800

    Сортировка выборкой

--- src/test/java/lessons/array/SortSelectedTest.java
+++ src/test/java/lessons/array/SortSelectedTest.java
@@ -5,6 +5,22 @@ import org.junit.Assert;
 
 public class SortSelectedTest {
     @Test
+    public void whenSort5() {
+        int[] data = new int[] {9, 14, 1, 20, 95};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 9, 14, 20, 95};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSort3() {
+        int[] data = new int[] {3, 4, 10};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {3, 4, 10};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
     public void whenSort() {
         int[] data = new int[] {3, 4, 1, 2, 5};
         int[] result = SortSelected.sort(data);
