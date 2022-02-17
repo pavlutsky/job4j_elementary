6ac7c67e50eadca05793c63f3834e14a97118963
pavlutsky <pavlutsky@mail.ru>
2022-02-17 15:51:01 +0800

    Поиск минимума в диапазоне.

---
+++ src/main/java/lessons/array/MinDiapason.java
@@ -0,0 +1,13 @@
+package lessons.array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int i = start; i <= finish; i++) {
+            if (min >= array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
