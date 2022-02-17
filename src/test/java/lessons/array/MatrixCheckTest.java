b186bddc040aa84370d31c9f3cfc96c86cd40ad9
pavlutsky <pavlutsky@mail.ru>
2022-02-17 22:01:06 +0800

    Проверка матрицы.

--- src/test/java/lessons/array/MatrixCheckTest.java
+++ src/test/java/lessons/array/MatrixCheckTest.java
@@ -1,7 +1,7 @@
 package lessons.array;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class MatrixCheckTest {
     @Test
@@ -29,14 +29,38 @@ public class MatrixCheckTest {
     }
 
     @Test
-    public void whenHasNoMonoHorizontal() {
+    public void whenDiagonalFullX() {
         char[][] input = {
-                {' ', ' ', ' '},
-                {'X', ' ', 'X'},
-                {' ', ' ', ' '},
+                {'X', ' ', ' '},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'},
         };
-        int row = 1;
-        boolean result = MatrixCheck.monoHorizontal(input, row);
-        Assert.assertFalse(result);
+        char[] result = MatrixCheck.extractDiagonal(input);
+        char[] expected = {'X', 'X', 'X'};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiagonalFullOne() {
+        char[][] input = {
+                {'1', ' ', ' '},
+                {' ', '1', ' '},
+                {' ', ' ', '1'},
+        };
+        char[] result = MatrixCheck.extractDiagonal(input);
+        char[] expected = {'1', '1', '1'};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiagonalMix() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'Y', ' '},
+                {' ', ' ', 'Z'},
+        };
+        char[] result = MatrixCheck.extractDiagonal(input);
+        char[] expected = {'X', 'Y', 'Z'};
+        Assert.assertArrayEquals(expected, result);
     }
 }
