a45a0c0a90787d34d2df4b22e8a25c847adbe3c8
pavlutsky <pavlutsky@mail.ru>
2022-02-17 14:33:09 +0800

    Слово заканчивается на ...

--- src/main/java/lessons/array/EndsWith.java
+++ src/main/java/lessons/array/EndsWith.java
@@ -2,10 +2,11 @@ package lessons.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = true;
         for (int i = 0; i < post.length; i++) {
-            return (word[word.length - 1 - i] == post[post.length - 1 - i]);
+            if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
+                return false;
             }
-    return result;
+            }
+        return true;
     }
 }
