5a519bacddb15411d11c2fef67b4c66ce74df4ac
pavlutsky <pavlutsky@mail.ru>
2022-02-17 12:34:27 +0800

    Перевернуть массив.

--- src/main/java/lessons/array/Turn.java
+++ src/main/java/lessons/array/Turn.java
@@ -3,17 +3,12 @@ package lessons.array;
 public class Turn {
 
     public static int[] back(int[] array) {
-        if (array.length <= 3) {
-            int temp = array[0];
-            array[0] = array[array.length - 1];
-            array[array.length - 1] = temp;
-        } else {
-            for (int i = 0; i < array.length / 2; i++) {
-                    int temp = array[i];
-                    array[i] = array[array.length - 1 - i];
-                    array[array.length - 1 - i] = temp;
-                }
-            }
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
         return array;
     }
-    }
+
+}
