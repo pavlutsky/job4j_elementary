f2a3ee075825851254717a223a2780b43ce229ae
pavlutsky <pavlutsky@mail.ru>
2022-02-17 15:42:03 +0800

    Поиск минимального числа в массиве.

---
+++ src/main/java/lessons/array/Min.java
@@ -0,0 +1,13 @@
+package lessons.array;
+
+public class Min {
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int index = 1; index < array.length; index++) {
+            if (min >= array[index]) {
+                min = array[index];
+            }
+        }
+        return min;
+    }
+}
