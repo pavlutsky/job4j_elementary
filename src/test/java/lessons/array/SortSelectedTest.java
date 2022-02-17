3015f9a733c901a8c53877c28ad9fab5e18facb4
pavlutsky <pavlutsky@mail.ru>
2022-02-17 17:43:38 +0800

    Сортировка выборкой

---
+++ src/test/java/lessons/array/SortSelectedTest.java
@@ -0,0 +1,14 @@
+package lessons.array;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SortSelectedTest {
+    @Test
+    public void whenSort() {
+        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+}
